ec80c65d15070191f36e6809f737d1af622ceb4d
lexlexpddx <lex.albrandt@gmail.com>
2025-05-13 13:11:25 -0700

    initial commit

---
+++ HW2.sql
@@ -0,0 +1,66 @@
+select *
+from student_classes sc
+limit 5;
+
+select d.dept_name, c.class_abbrev, c.class_description, c.class_credits
+from classes c
+join departments d on c.class_dept_id = d.dept_id
+where c.class_credits < 4
+order by d.dept_name, c.class_abbrev
+limit 3;
+
+
+create view classes_by_dept as
+select 
+	d.dept_name, c.class_abbrev, c.class_description, c.class_credits
+from classes c
+join departments d on c.class_dept_id = d.dept_id;
+
+select *
+from classes_by_dept
+where class_credits >= 4
+order by dept_name, class_abbrev;
+
+
+select 
+	d.dept_name,
+	c.class_abbrev,
+	c.class_description,
+	c.class_credits
+from
+	classes c
+left join departments d on
+	c.class_dept_id = d.dept_id
+where
+	c.class_credits < 4
+order by
+	d.dept_name,
+	c.class_abbrev;
+
+select 
+	d.dept_name,
+	c.class_abbrev,
+	c.class_description,
+	c.class_credits
+from
+	classes c
+left join departments d on
+	c.class_dept_id = d.dept_id
+where
+	c.class_credits >= 4
+order by
+	d.dept_name,
+	c.class_abbrev;
+
+select
+	d.dept_name,
+	s.student_family_name,
+	s.student_given_name
+from
+	students s
+left join departments d on
+	s.student_dept_id = d.dept_id
+order by
+	d.dept_name,
+	s.student_family_name,
+	s.student_given_name;
