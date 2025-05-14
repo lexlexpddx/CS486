dac3f57c35a2ff4a951c6515ea4b38093cededee
lexlexpddx <lex.albrandt@gmail.com>
2025-05-13 17:02:37 -0700

    new commit

--- HW2.sql
+++ HW2.sql
@@ -64,3 +64,23 @@ order by
 	d.dept_name,
 	s.student_family_name,
 	s.student_given_name;
+
+select
+	d.dept_name,
+	s.student_family_name,
+	s.student_given_name
+from
+	students s
+full outer join departments d on
+	s.student_dept_id = d.dept_id
+order by
+	d.dept_name,
+	s.student_family_name,
+	s.student_given_name;
+
+select s.student_given_name, s.student_family_name, c.class_abbrev
+from students s
+left join student_classes sc on s.student_id = sc.student_id
+join classes c on sc.class_id = c.class_id
+left join departments d on c.class_dept_id = d.dept_id
+order by s.student_given_name, s.student_family_name, c.class_abbrev, d.dept_name
